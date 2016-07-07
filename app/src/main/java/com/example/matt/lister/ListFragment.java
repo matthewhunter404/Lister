4b41e824198c0935d2baad21e4ff0d5e793b47fc
matt <matthewhunter404@gmail.com>
2016-07-07 10:56:48 +0200

    Moves the code for adding a line to a seperate method called by the onclick Handler

--- app/src/main/java/com/example/matt/lister/ListFragment.java
+++ app/src/main/java/com/example/matt/lister/ListFragment.java
@@ -117,33 +117,12 @@ public class ListFragment extends Fragment {
         titleText = (TextView) rootView.findViewById(R.id.TitleText);
         titleText.setText(listName);
 
-        //Code for the add list item option as a floating Action Button. This should maybe reuse code from edit popup window, but the key difference is mDisplayMainList.set vs mDisplayMainList.add which is at the centre of the method
+
         FloatingActionButton fab = (FloatingActionButton) rootView.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AlertDialog.Builder alert = new AlertDialog.Builder(mContext);
-                alert.setTitle("Title");
-                alert.setMessage("Message");
-                // Set an EditText view to get user input
-                final EditText input = new EditText(mContext);
-                input.setTextColor(Color.parseColor("#646464"));
-                alert.setView(input);
-                alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int pPosition) {
-
-                        mDisplayMainList.add(mDisplayMainList.size(), input.getText().toString());
-                        //((BaseAdapter) mRecyclerView.getAdapter()).notifyDataSetChanged();
-                    }
-                });
-
-                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        // Canceled.
-                    }
-                });
-
-                alert.show();
+                AddLine();
             }
         });
 
@@ -152,6 +131,34 @@ public class ListFragment extends Fragment {
 
         return rootView;
     }
+    //Code for the add list item option as a floating Action Button. This should maybe reuse code from edit popup window, but the key difference is mDisplayMainList.set vs mDisplayMainList.add which is at the centre of the method
+    public void AddLine()
+    {
+        AlertDialog.Builder alert = new AlertDialog.Builder(mContext);
+        alert.setTitle("Title");
+        alert.setMessage("Message");
+        // Set an EditText view to get user input
+        final EditText input = new EditText(mContext);
+        input.setTextColor(Color.parseColor("#646464"));
+        alert.setView(input);
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int pPosition) {
+
+                mDisplayMainList.add(mDisplayMainList.size(), input.getText().toString());
+                //((BaseAdapter) mRecyclerView.getAdapter()).notifyDataSetChanged();
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Canceled.
+            }
+        });
+
+        alert.show();
+
+    }
+
 
 }
 
