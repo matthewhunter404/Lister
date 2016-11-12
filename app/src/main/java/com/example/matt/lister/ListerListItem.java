fef40887af89a6fda997936ee0a017e72ab27340
Matthew <matthewhunter404@gmail.com>
2016-11-12 15:52:23 +0300

    Fills i nthe properties adn functinons of ListerListItem

--- app/src/main/java/com/example/matt/lister/ListerListItem.java
+++ app/src/main/java/com/example/matt/lister/ListerListItem.java
@@ -4,6 +4,20 @@ package com.example.matt.lister;
  * Created by MattsDesktop on 12/11/2016.
  */
 public class ListerListItem {
+    //ListerListItem is the object which stores each item of the List. Currently its basically just a wrapper for a string of text, but more might be added as needed.
+    //TODO analyse if having the list items as a separate object is really necessary.
+    private String ItemText; //The items text
 
-    
+    public ListerListItem(String pText) {
+        ItemText=pText;
+    }
+
+
+    public void getItemText(String pText) {
+        ItemText=pText;
+    }
+
+    public String setItemText(){
+        return ItemText;
+    }
 }
